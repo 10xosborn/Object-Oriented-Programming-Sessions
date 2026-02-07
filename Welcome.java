77ddc1c15211490435e9ab933a4f0df8e850a7b8
10xosborn <osborntulasi@gmail.com>
2026-02-07 04:28:13 +0000

    Added Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+
+
+    public static void printWelcome() {
+
+        System.out.println("Welcome to Java methods");
+    }
+
+    public static void main(String[] args) {
+        printWelcome();
+    }
+}
